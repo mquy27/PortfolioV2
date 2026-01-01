d2c9c7e7a8a923f8262979febe0f48586bc41310
mquy27 <leminhquy737@gmail.com>
2026-01-02 00:21:57 +0700

    feat: creating a contactme

---
+++ src/components/ContactMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactMe = () => {
+    return (
+        <section id="contact" className="w-full min-h-screen px-8 lg:px-24 py-20 relative">
+            <h1 className="text-amber-100 text-4xl md:text-5xl font-gochi font-bold text-center mb-16">Contact Me</h1>
+        </section>
+    );
+};
+
+export default ContactMe;
